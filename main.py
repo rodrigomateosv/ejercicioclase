8fbe73d50e90cb8fae5d9eec6fcf144fee4106db
Rodrigo Mateos <114070557+rodrigomateosv@users.noreply.github.com>
2022-10-19 07:05:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def ejercicio():
+    lista = ["P" , "t"]
+    lista.insert(1,"y")
+    lista.append("h")
+    lista.append("o")
+    lista.append("n")
+
+ #TO DO
+  assert"".join(lista) == "Python"
